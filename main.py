42f2d4a9c05a69e0e5a9cf5564e4dc001d7e56dd
dewie102 <zack.romasz@tlgcohort.com>
2023-11-03 21:47:16 +0000

    Changed up the json a bit, need to implement it now

--- main.py
+++ main.py
@@ -27,32 +27,7 @@ def showStatus():
 inventory = []
 
 # A dictionary linking a room to other rooms
-rooms = {
-
-            'Hall' : {
-                  'south' : 'Kitchen',
-                  'east'  : 'Dining Room',
-                  'item'  : 'key'
-                },
-
-            'Kitchen' : {
-                  'north' : 'Hall',
-                  'item'  : 'monster',
-                },
-            'Dining Room' : {
-                  'west' : 'Hall',
-                  'south': 'Garden',
-                  'item' : 'potion',
-                  'north' : 'Pantry',
-               },
-            'Garden' : {
-                  'north' : 'Dining Room'
-               },
-            'Pantry' : {
-                  'south' : 'Dining Room',
-                  'item' : 'cookie',
-            }
-         }
+#rooms = 
 
 #start the player in the Hall
 currentRoom = 'Hall'
